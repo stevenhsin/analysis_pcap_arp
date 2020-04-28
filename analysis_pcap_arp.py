8962b4514d22b5b9388412498e9d5d29d736a2f4
stevenhsin <steven.hsin@stonybrook.edu>
2020-04-28 17:26:01 -0400

    Prints out first ARP request-response pair

--- analysis_pcap_arp.py
+++ analysis_pcap_arp.py
@@ -8,23 +8,48 @@ def main():
     f = open(file_name, 'rb')
     pcap = dpkt.pcap.Reader(f)
     read_pcap_arp(pcap)
-    print(arp_packets[0][12:14])  # ARP
-    print(arp_packets[0][16:18])  # IP Protocol
-    print(arp_packets[0][20:22])  # Opcode
-    print(arp_packets[1][20:22])
-
-    print(arp_packets[0][22:28])  # Sender Mac Address
-    print(arp_packets[0][32:38])  # Target Mac Address
-    print(arp_packets[1][22:28].hex())  # Sender Mac Address
-
-    print("Sender MAC Address: " + get_mac_addr(arp_packets[1][22:28].hex()))  # works in returning MAC Address with proper formatting
-
-    print(arp_packets[1][32:38].hex())  # Target Mac Address
-
-    print("Hardware Size: " + str(arp_packets[0][18]))
-    print(get_ip(arp_packets[0][28:32]))  # Sender IP Address
-    print(get_ip(arp_packets[0][38:42]))  # Target IP Address
+    sort_arp()
     print(len(arp_packets))
+
+    print_arp_request()
+    print_arp_response()
+
+
+# prints out the first ARP request packet
+def print_arp_request():
+    print("-------------------------------------------ARP Request-------------------------------------------")
+    print("Hardware Type: " + str(int.from_bytes(arp_requests[0][14:16], "big")))
+    print("Protocol Type: 0x" + str(arp_requests[0][16:18].hex()))
+    print("Hardware Size: " + str(arp_requests[0][18]))
+    print("Protocol Size: " + str(arp_requests[0][19]))
+    print("Opcode: request (" + str(int.from_bytes(arp_requests[0][20:22], "big")) + ")")
+    print("Sender MAC Address: " + get_mac_addr(arp_requests[0][22:28].hex()))
+    print("Sender IP Address: " + get_ip(arp_requests[0][28:32]))
+    print("Target MAC Address: " + get_mac_addr(arp_requests[0][32:38].hex()))
+    print("Target IP Address: " + get_ip(arp_requests[0][38:42]))
+
+
+# prints out the first ARP response packet
+def print_arp_response():
+    print("-------------------------------------------ARP Response-------------------------------------------")
+    print("Hardware Type: " + str(int.from_bytes(arp_responses[0][14:16], "big")))
+    print("Protocol Type: 0x" + str(arp_responses[0][16:18].hex()))
+    print("Hardware Size: " + str(arp_responses[0][18]))
+    print("Protocol Size: " + str(arp_responses[0][19]))
+    print("Opcode: response (" + str(int.from_bytes(arp_responses[0][20:22], "big")) + ")")
+    print("Sender MAC Address: " + get_mac_addr(arp_responses[0][22:28].hex()))
+    print("Sender IP Address: " + get_ip(arp_responses[0][28:32]))
+    print("Target MAC Address: " + get_mac_addr(arp_responses[0][32:38].hex()))
+    print("Target IP Address: " + get_ip(arp_responses[0][38:42]))
+
+
+# sorts the ARP packets into request and response packets
+def sort_arp():
+    for arp in arp_packets:
+        if arp[20:22] == b'\x00\x01':
+            arp_requests.append(arp)
+        else:
+            arp_responses.append(arp)
 
 
 # returns the source/destination IP address
@@ -61,6 +86,8 @@ def read_pcap_arp(pcap):
 
 all_packets = []
 arp_packets = []
+arp_requests = []
+arp_responses = []
 
 if __name__ == '__main__':
     main()
